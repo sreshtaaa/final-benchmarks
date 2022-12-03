0f57e70b189e6876786b7e704decd49a0c1c573a
Sreshtaa <sreshtaa_rajesh@brown.edu>
2022-12-03 16:38:31 -0500

    some edits to benchmarks

--- benchmarks/const-prop-reassoc.lisp
+++ benchmarks/const-prop-reassoc.lisp
@@ -2,13 +2,14 @@
     (if (= 0 len)
         ()
         (pair elt 
-              (make-pair (- 1 len) elt))))
+              (make-pair (- len 1) elt))))
 
 (define (silly-ident x) (+ 1 (+ -1 (+ 1 (+ -1 (+ 1 (+ -1 (+ 1 (+ -1 (+ 1 (+ -1 (+ 1 (+ -1 x)))))))))))))
 
 (define (map-silly-ident l) 
     (if (empty? l) 
         () 
-        (pair (silly-ident (left l)) (map-silly-ident (right l)))))
+        (pair (silly-ident (left l)) 
+              (map-silly-ident (right l)))))
 
 (print (map-silly-ident (make-pair 200 5)))
